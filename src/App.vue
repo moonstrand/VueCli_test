67bc812a35397b0534ea11a0c99ecc2d7758c2d0
William <willam3305@gmail.com>
2022-06-29 14:56:16 +0800

    error version

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,9 @@
 <template>
-  <div>
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-    <button type="button" class="btn btn-primary">Primary</button>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
   </div>
+  <router-view />
 </template>
 
 <style lang="scss">
