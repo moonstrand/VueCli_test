de395d858b35d4c297438623fd7da3a7f6469538
William <willam3305@gmail.com>
2022-06-29 14:50:18 +0800

    update APP.vue & About.vue, creat ModuleCard.vue.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+    <card></card>
   </div>
 </template>
+<script>
+import card from '@/components/ModuleCard.vue'
+
+export default {
+  components: {
+    card
+  }
+}
+</script>
